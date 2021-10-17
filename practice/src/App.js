94fefd57f5b14acac10d2bec1d2bbe8b8aed060f
KimMinchan95 <hialscks@gmail.com>
2021-10-18 00:00:48 +0900

    update: GlobalStyle & Header

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,7 +1,12 @@
-function App() {
+import GlobalStyles from './GlobalStyles';
+import Header from './components/Header';
+
+function App () {
   return (
-    <div className="App">
-    </div>
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
   );
 }
 
